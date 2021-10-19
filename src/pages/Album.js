39639195083c29d19900450c3e20202f917a29fb
guilhermecd15 <guilhermecd15@gmail.com>
2021-10-19 07:55:34 -0300

    1. Crie as rotas necessárias para a aplicação

---
+++ src/pages/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <h1> Estamos na page Album </h1>
+      </div>
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        <h1> Estamos na page Search </h1>
+      </div>
+    );
+  }
+}
+
+export default Search;
