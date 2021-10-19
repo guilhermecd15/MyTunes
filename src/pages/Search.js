a1970e581b0519f8925a2645d84be555792898e4
guilhermecd15 <guilhermecd15@gmail.com>
2021-10-19 10:39:54 -0300

    5. Crie o formulário para pesquisar artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nameArtist: '',
+      buttonSearchDisabled: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => { this.Error(); });
+  }
+
+  Error() {
+    const { nameArtist } = this.state;
+    const minCaracter = 2;
+    if (nameArtist.length >= minCaracter) {
+      return this.setState({ buttonSearchDisabled: false });
+    }
+    return this.setState({ buttonSearchDisabled: true });
+  }
+
   render() {
+    const { buttonSearchDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1> Estamos na page Search </h1>
+        <form>
+          <label htmlFor="search-artist-label">
+            <input
+              type="text"
+              placeholder="Digite o nome do artista..."
+              data-testid="search-artist-input"
+              onChange={ this.onInputChange }
+              name="nameArtist"
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ buttonSearchDisabled }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
