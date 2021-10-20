e87b9e31340de9a21e6c5771a413bee8933852c4
guilhermecd15 <guilhermecd15@gmail.com>
2021-10-20 14:46:46 -0300

    8 - Mecanismo para adicionar músicas favoritas

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loadingFavorite: false,
+    };
+  }
+
+  checked = async ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { props: { song: trackId } } = this;
+    this.setState({ loadingFavorite: true });
+    await addSong(trackId);
+    this.setState({
+      loadingFavorite: false,
+      favoriteChecked: value,
+    });
+  }
+
   render() {
-    const {
-      props:
-        { song:
-          { previewUrl, trackName } },
+    const { props: { song:
+          { previewUrl, trackName, trackId } },
     } = this;
+    const { loadingFavorite, favoriteChecked } = this.state;
+    if (loadingFavorite) {
+      return <Loading />;
+    }
     return (
       <div>
         <p>{ trackName }</p>
@@ -18,6 +41,16 @@ class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="favorita">
+          <input
+            type="checkbox"
+            data-testid={ `checkbox-music-${trackId}` }
+            onClick={ this.checked }
+            checked={ favoriteChecked }
+            id="favorita"
+          />
+          Favorita
+        </label>
       </div>
     );
   }
@@ -27,6 +60,7 @@ MusicCard.propTypes = {
   song: PropTypes.shape({
     previewUrl: PropTypes.string.isRequired,
     trackName: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   }).isRequired,
 };
 
